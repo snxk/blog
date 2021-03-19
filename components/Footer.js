4a20c17aa20b8d72ce97e3b79fa8e423f3a776ac
Sayan Mallick <snyxmk@gmail.com>
2021-03-19 11:28:44 +0530

    Clean up And Added Eslint

--- components/Footer.js
+++ components/Footer.js
@@ -9,9 +9,9 @@ export default function Footer() {
         <div className="flex mb-3 space-x-4">
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
           <SocialIcon kind="github" href={siteMetadata.github} size="6" />
-          <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" />
+          {/* <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" />
           <SocialIcon kind="youtube" href={siteMetadata.youtube} size="6" />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
+          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" /> */}
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
         </div>
         <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
@@ -23,7 +23,10 @@ export default function Footer() {
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
+            This beautiful Nextjs Theme was made by timlrx with{' '}
+            <span role="img" aria-label="heart.emoji">
+              ❤️
+            </span>
           </Link>
         </div>
       </div>
